be7c9c3be628481d2886d9725c86f231f95903e5
patiuaarondhvsu <2021305281@dhvsu.edu.ph>
2025-10-27 11:21:23 +0800

    added some practice access and authorization

--- controlFlow/control_flow.js
+++ controlFlow/control_flow.js
@@ -50,3 +50,18 @@ let isAuthenticated = true;
 let authenticationStatus = isAuthenticated ? "Authenticated" : "Not authenticated";
 
 console.log("Authentication Status:", authenticationStatus);
+
+let access = "employee";
+let accessStatus;
+
+if (access === "employee") {
+    accessStatus = " You are authorized to have access to Dietary Services";
+} else if (access === "enrolled_member") {
+        accessStatus = "You are authorized to have access to Dietary Services and one-on-one interaction with a dietician.";
+} else if (access === "subscriber") {
+        accessStatus = "You are authorized to have partial access to facilitate Dietary Services only";
+} else {
+        accessStatus = "You need to enroll or at least subscribe first to avail this facility.";
+}
+
+console.log("Access Status:", accessStatus);
